f4a9c11f1896fee5f6c8fa06937ad1d56fbc824f
Petr <peachtrees2009@gmail.com>
2024-05-23 16:30:50 +0300

    23052024_1_DevServer_and_Source_map

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,28 +5,31 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from  'webpack';
+import type {Configuration as DevServerCnfiguration} from 'webpack-dev-server'
 
 type Mode = 'production' | 'development';
 
 interface EnvVariables {
-  mode: Mode
+  mode: Mode,
+  port: number
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development';
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
+    entry: path.resolve(__dirname, 'src', 'index.ts'), //путь до входного файла, до точки входа в наше приложение
+    output: { //тут задаем куда происходить сборка
       path: path.resolve(__dirname, 'build'),
-      filename: '[name].[contenthash].js',
+      filename: '[name].[contenthash].js', //для filename можно использовать шаблоны чтобы избежать проблем кеширования в браузере
       clean: true
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
-      new webpack.ProgressPlugin()
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }), //подставляет скрипты которые получаются в результате сборки в нашу html-ку
+      new webpack.ProgressPlugin() //показывает в процентах насколько прошла сборка (помни что в больших проектах он может замедлять сборку и посему их тоже можно исключать используя наш маркер isDev)
     ],
     module: {
-      rules: [
+      rules: [ //тут у нас лоудеры которые както обрабатывают файлы с разными расширениями
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
@@ -35,8 +38,13 @@ export default (env: EnvVariables) => {
       ],
     },
     resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    }
+      extensions: ['.tsx', '.ts', '.js'], //тут расширения файлов с исходным кодом
+    },
+    devtool: isDev && 'inline-source-map', //делаем source map 
+    devServer: isDev ? {  //настраиваем devServer
+      port: env.port ?? 3000,
+      open: true
+    }:undefined
 
   }
 
